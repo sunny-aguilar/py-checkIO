a33b6b1dde0f83f36a7e266e87dd43a115b7d670
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-09-03 20:13:52 -0700

    io:

--- checkIO.py
+++ checkIO.py
@@ -1,3 +1,15 @@
 
+def index_power(array: list, n: int) -> int:
+    """
+        Find Nth power of the element with index N.
+    """
+    listLength = len(list)
+    if listLength > 0:
+        calc = 0
+        for i in list:
+            calc *= list[n]
+
+    else:
+        return -1
 
 
