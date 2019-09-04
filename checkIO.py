64128a0fa30027720afcb0d8961b2bdd622d9852
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-09-03 20:24:18 -0700

    io

--- checkIO.py
+++ checkIO.py
@@ -1,8 +1,8 @@
 
 
 def main():
-    alist = [1, 2, 3]
-    n = 2
+    alist = [1, 2, 3, 4, 5]
+    n = 4
     index_power(alist, n)
 
 
@@ -12,11 +12,12 @@ def index_power(my_list, n):
     """
     list_length = len(my_list)
     if list_length > 0:
-        calc = 0
+        calc = 1
         print(my_list[n])
         for i in my_list:
             #print('11')
             calc = calc * my_list[n]
+
         return calc
     else:
         return -1
