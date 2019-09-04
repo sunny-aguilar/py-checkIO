157bffef9d200931c023154656a8d1048d87ce25
Sunny Aguilar <sunny.aguilar@gmail.com>
2019-09-03 23:20:30 -0700

    io

--- checkIO.py
+++ checkIO.py
@@ -1,26 +1,27 @@
 # practice problems
+# from checkIO
 
 
-def main():
-    alist = [1, 2, 3, 4]
-    n = 2
-    index_power(alist, n)
+# def main():
+#     alist = [1, 2, 3, 4]
+#     n = 2
+#     index_power(alist, n)
+#
+#
+# def index_power(my_list, n):
+#     """
+#         Find Nth power of the element with index N.
+#     """
+#     list_length = len(my_list)
+#     if list_length > 0:
+#         calc = 1
+#
+#         for i in range(n):
+#             calc = calc * my_list[n]
+#         print(calc)
+#         return calc
+#     else:
+#         return -1
+#
 
-
-def index_power(my_list, n):
-    """
-        Find Nth power of the element with index N.
-    """
-    list_length = len(my_list)
-    if list_length > 0:
-        calc = 1
-
-        for i in range(n):
-            calc = calc * my_list[n]
-        print(calc)
-        return calc
-    else:
-        return -1
-
-
-main()
+# main()
